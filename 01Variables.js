e42ae2d8499095494e856098c04e8afb869751ab
sakets7069 <workmail.saketsh7069@gmail.com>
2022-02-09 14:51:28 +0530

    First Commit

---
+++ 01Variables.js
@@ -0,0 +1,15 @@
+// Var, let, const
+var fullName = "Saket Sharma";
+console.log(fullName);
+
+var isLoggedIn = true;
+console.log(isLoggedIn);
+
+var loggedCount = 34;
+console.log("loggedCount-", loggedCount);
+
+
+var paymentMode;
+
+paymentMode = "Credit Card";
+console.log(paymentMode);
